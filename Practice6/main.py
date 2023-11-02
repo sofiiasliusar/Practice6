306b4dc286ebac779e8d068448e61ca526393095
Sofiia Sliusar <144224845+sofiiasliusar@users.noreply.github.com>
2023-11-02 12:24:24 +0200

    Add files via upload

--- Practice6/main.py
+++ Practice6/main.py
@@ -1,16 +1,25 @@
 import math
 
-file = open('input.txt', 'r')
-x = file.read()
-x = float(x)
+with open("input.txt", "r") as in_text:
+    x =float(in_text.read())
+def calculatec(x):
+    c = math.sin(math.cos(x)-1)*(math.e**2)
+    return c
+
+def calculatey(x):
+    y = 1 + math.log(3 + abs(x))
+    return y
+
+c = calculatec(x)
+y = calculatey(x)
+
 print("x = ", x)
+print("c = ", c)
+print("y = ", y)
 
-c = math.sin(math.cos(x) - 1)*(math.e**2)
-y = 1 + math.log(3 + abs(x))
+with open("output.txt", "w") as out_text:
+    out_text.write(f"{x}\n")
+    out_text.write(f"{c}\n")
+    out_text.write(f"{y}\n")
 
-print(c)
-print(y)
 
-out_text = c, y
-f = open('output.txt', 'w')
-f.write(out_text)
